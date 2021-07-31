efe668b1b42b47ee5d5dbdddfe388da6a9491b11
jasonronalddavis <jdavis@c.ringling.edu>
2021-07-31 02:37:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "Barnes is the champion";
+// innerText = document.createTextNode("LUKAAAAA is the champion");
+// newHeader.appendChild(innerText);
+document.body.appendChild(newHeader);
